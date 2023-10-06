df6f23978ed4d304f6789e62deeff82351c8657f
Pola Borkowska <poolaborkowska@gmail.com>
2023-10-06 20:23:49 +0200

    m

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ if __name__=='__main__':
 
     allData = {}
 
+    # decorate the functions with decorators and save the decorated functions
     function1 = decoratorSavingResults(decoratorTime(func2), allData,
                                     con, 'functionsResults', cur)
     function2 = decoratorSavingResults(decoratorTime(func1), allData,
                                     con, 'functionsResults', cur)
     function3 = decoratorSavingResults(decoratorTime(lottery), allData, con , 'functionsResults', cur)
 
+    # call the decorated functions multiple times
     for i in range(3):
         function1(8, 10)
         function1(2, 0)
         function3(5, 100)
 
+    # print results to check them quickly
     print(allData)
     con.commit()
     con.close()
